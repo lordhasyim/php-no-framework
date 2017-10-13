9fbbb6ba07b884b0ff800c2f1c181bb6daa884e4
Hasyim <lordhasyim@gmail.com>
2017-10-13 18:19:14 +0700

    first commit

---
+++ src/Bootstrap.php
@@ -0,0 +1,27 @@
+<?php declare(strict_types=1);
+namespace Example;
+
+require __DIR__ . '/../vendor/autoload.php';
+error_reporting(E_ALL);
+$environment = 'development';
+
+/**
+ * Register the error handler
+ */
+
+$whoops = new \Whoops\Run;
+if ($environment !== 'production') {
+    $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
+} else {
+    $whoops->pushHandler(function($e) {
+        echo 'Todo : Friendly error page';
+    });         
+}
+
+$whoops->register();
+throw new \Exception;
+
+
+
+echo 'hello_world';
+
