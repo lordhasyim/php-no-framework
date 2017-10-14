b36dea820e76305c5fee274c9f8b929679e3ce38
Hasyim <lordhasyim@gmail.com>
2017-10-14 14:11:39 +0700

    add FastRoute for Routing

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -17,21 +17,36 @@ if ($environment !== 'production') {
         echo 'Todo : Friendly error page';
     });         
 }
+$whoops->register();
 
 $request = new \Http\HttpRequest($_GET, $_POST, $_COOKIE, $_FILES, $_SERVER);
 $response = new \Http\HttpResponse;
-$whoops->register();
-$content = '<h1>hello bos</h1>';
-$response->setContent('404 - page not found');
-$response->setStatusCode(404);
-foreach ($response->getHeaders() as $header) {
-    header($header, false);
+
+$routeDefinitionCallback = function (\FastRoute\RouteCollector $r) {
+    $routes = include('Routes.php');
+    foreach ($routes as $route){
+        $r->addRoute($route[0], $route[1], $route[2]);
+    }
+};
+
+$dispatcher = \FastRoute\simpleDispatcher($routeDefinitionCallback);
+
+$routeInfo = $dispatcher->dispatch($request->getMethod(), $request->getPath());
+switch ($routeInfo[0]) {
+    case \FastRoute\Dispatcher::NOT_FOUND:
+        $response->setContent('404 - Page not found');
+        $response->setStatusCode(404);
+        break;
+    case \FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
+        $response->setContent('405 - Method not allowed');
+        $response->setStatusCode(405);
+        break;
+    case \FastRoute\Dispatcher::FOUND:
+        $handler = $routeInfo[1];
+        $vars    = $routeInfo[2];
+        call_user_func($handler, $vars);
+        break;
 }
-echo $response->getContent();
-
-
-//throw new \Exception;
-
 
 
 echo 'hello_world';
